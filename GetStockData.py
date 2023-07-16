7a09e8befe241a6c9ce1800a17d7a7d0fb78d7ad
Code_with_Siddharth <88105873+SiddharthRaoK@users.noreply.github.com>
2023-07-17 00:28:57 +0530

    Delete GetStockData.py

--- GetStockData.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-def GetCurrentPrice(tickername):
-    stock = yf.Ticker(tickername)
-    price = stock.history(period='1min')['Close'].iloc[-1]
-    return price
-
-tickername = 'AAPL'
-print(GetCurrentPrice(tickername.upper()))
